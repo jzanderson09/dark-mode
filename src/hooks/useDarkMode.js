55388fc53d803519a177be04f2702bc21fb1b5f4
Lambda_School_Loaner_229 <lambda_school_loaner_229@Lambda-School-Loaner-229s-MacBook-Pro.local>
2020-01-21 21:18:44 -0700

    Initial push -- Set up useLocalStorage hook and hooks directory, per Step 1 of the README. Currently working on useDarkMode hook, per Step 2.

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import useLocalStorage from './useLocalStorage.js';
+
+const useDarkMode = () => {
+
+};
+
+//local storage key: darkMode
+// value: either true or false
